288e4dfc1f47555d63273f5790fcafefad2076ac
dev-jbam <dev.jbam@gmail.com>
2019-02-22 18:09:31 +0900

    add few programmers problems

--- README.md
+++
@@ -1,4 +0,0 @@
-같이 공부해요~
-
-https://www.acmicpc.net/user/leejb905
-
--- src/programmers/main.cpp
+++ src/programmers/main.cpp
@@ -4,7 +4,6 @@
 #include <cstdio>
 #include <iostream>
 
-extern int solution(vector<vector<int>> baseball);
 
 
 void call_vvi_solution(){
@@ -12,24 +11,28 @@ void call_vvi_solution(){
     getline(cin, input);
     StringParser<int> sparser;
     vector<vector<int> > vvi = sparser.vector_vector_parser(input, ',');
-    solution(vvi);
+    // solution(vvi);
 }
 
 
 void call_vi_solution(){
-    string input;
-    getline(cin, input);
+    int n  = 0 ;
+    cin >> n;
+    char a;
+    cin >> a;
+    string lost, reserve;
+    getline(cin, lost);
+    getline(cin,reserve);;
     StringParser<int> sparser;
-    vector<int> vi = sparser.vector_parser(input,',');
-    // solution(vi);
+    vector<int> vi = sparser.vector_parser(lost,',');
+    vector<int> vi2 = sparser.vector_parser(reserve,',');
+    // solution(n,vi,vi2);
 }
 
 int main(){
 
     freopen("/home/joon/sources/algorithm/build/input.txt", "r", stdin);
-    
-    call_vvi_solution();
-    
+
     return 0;
 }
 
